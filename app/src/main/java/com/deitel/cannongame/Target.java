38056cd4dd190350eadd2de13ad1aecae367b7ce
JvArruda <joaovictorcostaarruda@gmail.com>
2025-11-27 23:59:09 -0300

    FEAT: Implementação da lógica de projéteis e alvos (Cannon, Target, Blocker)

---
+++ app/src/main/java/com/deitel/cannongame/Target.java
@@ -0,0 +1,22 @@
+package com.deitel.cannongame;
+
+public class Target extends GameElement {
+
+    private int hitReward; // the hit reward for this target
+
+    // constructor
+    public Target(CannonView view, int color, int hitReward, int x, int y,
+                  int width, int length, float velocityY) {
+
+        super(view, color, CannonView.TARGET_SOUND_ID, x, y, width, length,
+                velocityY);
+
+        this.hitReward = hitReward;
+    }
+
+    // returns the hit reward for this Target
+    public int getHitReward() {
+        return hitReward;
+    }
+}
+}
