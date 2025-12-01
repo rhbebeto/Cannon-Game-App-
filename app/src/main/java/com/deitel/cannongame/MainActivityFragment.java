28143b03b6481e88a73f62afc5ddafb8bc85fa66
Bebeto <robertohenriqueduarte05@gmail.com>
2025-12-01 20:00:00 -0300

    fix: Altera biblioteca obsoleta que ta presente no livro para uma atualizada.

--- app/src/main/java/com/deitel/cannongame/MainActivityFragment.java
+++ app/src/main/java/com/deitel/cannongame/MainActivityFragment.java
@@ -1,45 +1,47 @@
 // MainActivityFragment.java
- // MainActivityFragment creates and manages a CannonView
+// MainActivityFragment creates and manages a CannonView
 package com.deitel.cannongame;
 
- import android.os.Bundle;
- import android.support.v4.app.Fragment;
- import android.view.LayoutInflater;
- import android.view.View;
- import android.view.ViewGroup;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-    public class MainActivityFragment extends Fragment {
-        private CannonView cannonView; // custom view to display the game
+public class MainActivityFragment extends Fragment {
+    private CannonView cannonView; // custom view to display the game
 
-                 // called when Fragment's view needs to be created
-                 @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                          Bundle savedInstanceState) {
-                     super.onCreateView(inflater, container, savedInstanceState);
+    // called when Fragment's view needs to be created
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
 
-                     // inflate the fragment_main.xml layout
-                     View view =
-                             inflater.inflate(R.layout.fragment_main, container, false);
-                     // get a reference to the CannonView
-                     cannonView = (CannonView) view.findViewById(R.id.cannonView);
-                     return view;
-                 }
+        // inflate the fragment_main.xml layout
+        View view =
+                inflater.inflate(R.layout.fragment_main, container, false);
+        // get a reference to the CannonView
+        cannonView = (CannonView) view.findViewById(R.id.cannonView);
+        return view;
+    }
 
-                 // set up volume control once Activity is created
-                 @Override
-                 public void onActivityCreated(Bundle savedInstanceState) {
-                     super.onActivityCreated(savedInstanceState);
-                 }
+    // set up volume control once Activity is created
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 
-                 // when MainActivity is paused, terminate the game
-                 @Override
-                 public void onPause() {
-                     super.onPause();
-                     cannonView.stopGame(); // terminates the game
-                 }
+    // when MainActivity is paused, terminate the game
+    @Override
+    public void onPause() {
+        super.onPause();
+        cannonView.stopGame(); // terminates the game
+    }
 
-                 // when MainActivity is paused, MainActivityFragment releases resources
-                 @Override
-                 public void onDestroy() {super.onDestroy();
-                      cannonView.releaseResources();
-                      }
-                        }
+    // when MainActivity is paused, MainActivityFragment releases resources
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cannonView.releaseResources();
+    }
+}
